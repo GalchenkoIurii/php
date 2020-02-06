1dd52b06b97b49d67b1c10edfa0471fa62943625
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-02-06 21:43:01 +0200

    changed mail.php

--- mail.php
+++ mail.php
@@ -1,5 +1,11 @@
 <?php
 
-if(mail('galchenkou@gmail.com', 'some subject', 'some message')) {
+$to       = 'galchenkou@gmail.com';
+$subject  = 'some subject';
+$message  = 'some message';
+$headers  = "From: test@gmail.com\nReply-To: test@gmail.com\n";
+$headers .= "Content-Type: text/html; charset='utf-8'\n";
+
+if(mail($to, $subject, $message, $headers)) {
     echo "mail has been sent";
 }
