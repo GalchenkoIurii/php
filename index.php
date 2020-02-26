69e95379ab861141f71a03490b7a39d35b8841f4
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-02-26 17:38:44 +0200

    added form

--- index.php
+++ index.php
@@ -6,12 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Working with json data</title>
+    <?php
+    //require_once 'mail.php';
+    ?>
 </head>
 <body>
-<?php
-//require_once 'mail.php';
-?>
+<div>
+    <p id="greeting">Hi!!!</p>
+</div>
+<div>
+    <form action="form.php" method="POST">
+        <p>Enter your Name: <input type="text" name="name" value=""></p>
+        <p>Enter your Surname: <input type="text" name="surname" value=""></p>
+        <p><input type="submit" value="Send"></p>
+    </form>
+</div>
 
-<script src=""></script>
+<script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+<script src="main.js"></script>
 </body>
 </html>
